9bd9c0b74a7ebc412b23db1807908e9361c256dd
Mahbub2001 <imahbubturza@gmail.com>
2022-08-02 18:00:50 +0600

    p1

---
+++ ARRAY/checking_array_or_not.js
@@ -0,0 +1,20 @@
+var a1 = {name: "John", age: 18};   
+var a2 = ["red", "green", "blue", "yellow"];
+var a3 = [1, 2, 3, 4, 5];
+var a4 = null;
+var a5 = 324; 
+
+
+// Testing if the variable is an array
+console.log(Array.isArray(a1));
+console.log(Array.isArray(a2));
+console.log(Array.isArray(a3));
+console.log(Array.isArray(a4));
+console.log(Array.isArray(a5));
+
+
+// // Testing the variables data type
+// console.log(typeof v1);
+// console.log(typeof v2);
+// console.log(typeof v3);
+// console.log(typeof v4);
