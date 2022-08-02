642741d06439eba3950204943626e94f0a4f639d
Mahbub2001 <imahbubturza@gmail.com>
2022-08-02 18:19:26 +0600

    p3

---
+++ first&last_element.js
@@ -0,0 +1,14 @@
+var array = [7, 9, 0, -2];
+console.log("First element is : " + array[0]);
+
+
+var array = ['hello','hi','he','she'];
+console.log("First element is : " + array[0]);
+
+
+var array = [7, 9, 0, -2];
+console.log("First element is : " + array[array.length-1]);
+
+
+var array = ['hello','hi','he','she'];
+console.log("Last element is : " + array[array.length-1]);
