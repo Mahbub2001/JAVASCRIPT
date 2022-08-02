d7db9a02af86e69b5b20a5113aaf89b2f669fb5e
Mahbub2001 <imahbubturza@gmail.com>
2022-08-02 18:12:30 +0600

    p2

---
+++ ARRAY/clone.js
@@ -0,0 +1,22 @@
+
+///
+var array = [23,77,33,566,33];
+array_copy = array.slice();
+console.log(array_copy);
+
+
+
+
+
+
+
+
+
+
+// OR,
+var array = ['mahbub', 'mahfuz', 'mahfuza','ferdowsh'];
+var array_copy=[];
+for (let i = 0; i < array.length; i++) {
+    array_copy[i] = array[i];  
+}
+console.log(array_copy);
