5caa7e62a479efb943501eeafe06272b76637b8c
Mahbub2001 <imahbubturza@gmail.com>
2022-08-03 00:45:21 +0600

    5

---
+++ ARRAY/input-dash-between_two_even.js
@@ -0,0 +1,14 @@
+var number = 23423423;
+var str = number.toString();
+var array = [str[0]];
+
+for (var i = 1; i < str.length; i++){
+    if (str[i -1] % 2 == 0 && str[i] % 2 == 0) {
+        array.push('-',str[i]);
+    }
+    else{
+        array.push(str[i]);
+    }
+   
+}
+console.log(array.join(''));
---
+++ ARRAY/join_between_arrays.js
@@ -0,0 +1,5 @@
+myName = ["rahim", "karim", "rahit", "tashin"];
+console.log(myName.toString());
+console.log(myName.join(','));
+console.log(myName.join(' '));
+console.log(myName.join('+'));
