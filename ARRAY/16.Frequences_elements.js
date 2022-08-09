d1a10ca0e30960f0d3d80b8dcd25639c1445b20a
Mahbub2001 <imahbubturza@gmail.com>
2022-08-09 23:27:56 +0600

    initial

--- ARRAY/16.Frequences_elements.js
+++ ARRAY/16.Frequences_elements.js
@@ -1,26 +1,25 @@
-const items2 = [1, 2, -2, 4, 5, 4, 7, 8, 7, 7, 71, 3, 6]
+const items2 = [1, 2, -2, 4, 5, 4, 7, 8, 7, 7, 71, 3, 6];
 
-function find_duplicate_in_array(array){
-const count = {}
-const result = []
+function find_duplicate_in_array(array) {
+  const count = {};
+  const result = [];
 
-array.forEach(item => {
+  array.forEach((item) => {
     if (count[item]) {
-       count[item] +=1
-       return
+      count[item] += 1;
+      return;
     }
-    count[item] = 1
-})
+    count[item] = 1;
+  });
 
-for (let i in count){
-    if (count[i] >=2){
-        result.push(i)
+  for (let i in count) {
+    if (count[i] >= 2) {
+      result.push(i);
     }
-}
+  }
 
-console.log(count)
-return result;
-
+  console.log(count);
+  return result;
 }
 
 find_duplicate_in_array(items2);
