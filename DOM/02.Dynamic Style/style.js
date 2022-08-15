12c038562c14eddb45f23dbbdb111e3da9f9eb3b
Mahbub2001 <imahbubturza@gmail.com>
2022-08-15 19:22:12 +0600

    initial

---
+++ DOM/02.Dynamic Style/style.js
@@ -0,0 +1,6 @@
+const h1 = document.getElementById("first");
+h1.style.color = "red";
+h1.style.backgroundColor = "yellow";
+h1.style.padding = "10px";
+h1.style.textAlign = "center";
+h1.style.border = "5px solid black";
---
+++ DOM/03.Add_Remove_class/cls.js
@@ -0,0 +1,5 @@
+const h1 = document.getElementById("first");
+
+h1.classList.add("third");//added the class..change background by css
+
+h1.classList.remove("third");//remove the class
