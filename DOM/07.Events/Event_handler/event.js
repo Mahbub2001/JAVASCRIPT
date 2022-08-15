e1822d21039542478ea57626596b1172bd093f53
Mahbub2001 <imahbubturza@gmail.com>
2022-08-15 22:35:43 +0600

    initial

---
+++ DOM/07.Events/Event_handler/event.js
@@ -0,0 +1,8 @@
+function makeRed()
+{
+    document.body.style.backgroundColor="red";
+}
+function makeBlue()
+{
+    document.body.style.backgroundColor="blue";
+}
---
+++ DOM/07.Events/event listener/event2.js
@@ -0,0 +1,4 @@
+const blueBtn = document.getElementById("blueBtn");
+blueBtn.addEventListener('click',function(){
+    document.body.style.backgroundColor="blue";
+});
