43def397855d6f01143e011646e19ea3929d757e
Mahbub2001 <imahbubturza@gmail.com>
2022-08-16 00:01:15 +0600

    initial

---
+++ Some DOM Projects/02.Love_Project/love.js
@@ -0,0 +1,7 @@
+document.getElementById('yes').addEventListener('click',function(){
+    document.getElementById('msg').innerText = "I LOVE YOU "
+});
+
+function loveletter(){
+    document.getElementById('msg').innerText="I LOVE YOU";
+}
