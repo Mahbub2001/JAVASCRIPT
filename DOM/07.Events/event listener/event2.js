3ae9290b4cdf66475e38745a0cd4b0fb896e5221
Mahbub2001 <imahbubturza@gmail.com>
2022-08-15 22:38:27 +0600

    initial

--- DOM/07.Events/event listener/event2.js
+++ DOM/07.Events/event listener/event2.js
@@ -1,4 +1,9 @@
 const blueBtn = document.getElementById("blueBtn");
-blueBtn.addEventListener('click',function(){
+blueBtn.addEventListener('click',function(){//this function name is callback function or annonymus function
     document.body.style.backgroundColor="blue";
+});
+
+const redBtn = document.getElementById("RedBtn");
+redBtn.addEventListener('click',function(){//this function name is callback function or annonymus function
+    document.body.style.backgroundColor="red";
 });
