b6f83ab4e1166f0a91046b9716731edc39c5e13e
Mahbub2001 <imahbubturza@gmail.com>
2022-08-16 00:57:33 +0600

    initial

--- Some DOM Projects/02.Love_Project/love.js
+++ Some DOM Projects/02.Love_Project/love.js
@@ -1,7 +1,14 @@
 document.getElementById('yes').addEventListener('click',function(){
-    document.getElementById('msg').innerText = "I LOVE YOU "
+    // document.getElementById('msg').innerText = "I LOVE YOU "
+    sentMsg("msg");
 });
 
 function loveletter(){
-    document.getElementById('msg').innerText="I LOVE YOU";
+    // document.getElementById('msg').innerText="I LOVE YOU";
+    sentMsg("msg");
+}
+
+//Easier way
+function sentMsg(id){
+    document.getElementById(id).innerText="I LOVE YOU";
 }
---
+++ Some DOM Projects/03.To_Do_List/to_do.js
@@ -0,0 +1,26 @@
+let count = 0;
+document.getElementById('input-btn').addEventListener("click",function(){
+    count++;
+    const inputValue = document.getElementById("input-value").value;
+    
+    const mainContainer = document.getElementById('content-container');
+    const tableContainer = document.createElement('tr');
+    tableContainer.innerHTML=`
+    <th scope="row">${count}</th> 
+    <td>${inputValue}</td>
+    <td>
+    <button class="btn btn-primary delete-btn">Delete</button>
+    <button class="btn btn-secondary">Edit</button>
+    </td>
+    `;
+    mainContainer.appendChild(tableContainer);
+    document.getElementById('input-value').value='';//empty the input box
+
+    const deleteBtn = document.getElementsByClassName("delete-btn")
+
+    for (let button of deleteBtn) {
+        button.addEventListener('click',function(event){
+            event.target.parentNode.parentNode.style.display="none"
+        });
+    }
+});
