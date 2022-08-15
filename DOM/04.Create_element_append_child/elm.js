5131bcc81b1883d087950cc84716bc76ec1c068a
Mahbub2001 <imahbubturza@gmail.com>
2022-08-15 21:40:47 +0600

    initial

---
+++ DOM/04.Create_element_append_child/elm.js
@@ -0,0 +1,4 @@
+const div = document.getElementById("half");
+const h3 = document.createElement("h3");
+h3.innerText = "I am from Bangladesh";
+div.appendChild(h3);
---
+++ DOM/06.Get_Set_Attribute/attr.js
@@ -0,0 +1,5 @@
+const div = document.getElementById("half");
+//find the name of attibutes
+// console.log(div.getAttribute("id"));
+//set attribute
+console.log(div.setAttribute("class","last"));//first is the type of attribute that you  want and then the name of the attribute that  you want.
