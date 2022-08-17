b3370203e7d3756ccc32c27cb00f6f313859d534
Mahbub2001 <imahbubturza@gmail.com>
2022-08-18 03:55:49 +0600

    initial

---
+++ Some DOM Projects/06.Simple Bank/bank.js
@@ -0,0 +1,52 @@
+
+document.getElementById('DepositBtn').addEventListener('click',function(){
+    const depositAmount = amountTaker();
+
+    const preMoney = document.getElementById('DepositTK');
+    const preMoneyString = preMoney.innerText;
+    const previousDepositAmount = parseFloat(preMoneyString);
+
+    const plus = depositAmount + previousDepositAmount;
+    preMoney.innerText = plus;
+
+    const totalAmount = bringTotalAmount();
+    const plus2 = depositAmount + totalAmount;
+    document.getElementById('totalTK').innerText = plus2;
+    
+});
+document.getElementById('WithdrawBtn').addEventListener('click',function(){
+    const WithdrawMoney = amountTaker();
+
+    const preDraw = document.getElementById('WithdrawTK');
+    const preDrawString= preDraw.innerText;
+    const previousWithdrawAmount = parseFloat(preDrawString);
+
+    const totalAmount = bringTotalAmount();
+
+    if (WithdrawMoney <= totalAmount) {
+        const plus = WithdrawMoney + previousWithdrawAmount;
+        preDraw.innerText = plus;
+    
+        const plus2 = totalAmount - WithdrawMoney;
+        document.getElementById('totalTK').innerText = plus2;
+    }
+    else{
+        alert("You haven't enough money")
+    }
+    
+});
+
+function amountTaker()
+{
+    const amountString = document.getElementById('amountInput').value;
+    const amount = parseFloat(amountString);
+    document.getElementById('amountInput').value = '';
+    return amount;
+}
+function bringTotalAmount()
+{
+    const getValue = document.getElementById('totalTK');
+    const getValueString = getValue.innerText;
+    const totalValue = parseFloat(getValueString);
+    return totalValue;
+}
